f5934a647b258af9b3c0b45110619a578e10086e
Diego Alvarado <daao87@gmail.com>
2013-07-10 15:53:42 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var buff = fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+var txt = buff.toString();
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
