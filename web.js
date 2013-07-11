b38980e2d9c9da6b56d94f7f0ca74594076d26f6
Diego Alvarado <daao87@gmail.com>
2013-07-11 09:07:00 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,21 +1,14 @@
-var fs = require('fs');
-
-var buff = fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
-var txt = buff.toString();
-
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(txt);
+    var fs = require('fs');
+    var buf = new Buffer(fs.readFileSync('file.html'), 'utf-8');
+    response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
